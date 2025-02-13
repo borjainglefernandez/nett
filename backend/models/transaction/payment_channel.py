754a69a262a6c459045c31e90982b4e15b9a833d
Borja Ingle-Fernandez <borja@mac.mynetworksettings.com>
2025-02-13 17:58:00 -0500

    fix: txn category and payment channel enums

--- backend/models/transaction/payment_channel.py
+++ backend/models/transaction/payment_channel.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
 class PaymentChannel(Enum):
-    IN_STORE = "In Store"
-    ONLINE = "Online"
-    OTHER = "Other"
+    IN_STORE = "in store"
+    ONLINE = "online"
+    OTHER = "other"
--- backend/utils/file_utils.py
+++ backend/utils/file_utils.py
@@ -2,7 +2,7 @@ import csv
 from enum import Enum
 
 # Function to read CSV and create Enum
-def create_enum_from_csv(file_path, column_name):
+def create_enum_from_csv(file_path, column_name, enum_name):
     # Read the CSV and extract the unique values from the specified column
     
     with open(file_path, newline='', encoding='utf-8') as csvfile:
@@ -11,6 +11,6 @@ def create_enum_from_csv(file_path, column_name):
         column_values = sorted(set(row[column_name] for row in reader))
 
     # Create the Enum dynamically
-    dynamic_enum = Enum(column_name, column_values)
+    dynamic_enum = Enum(enum_name, {value: value for value in column_values})
 
     return dynamic_enum
